c47c00dd026f1b1deb81df53e391f44cdc5653fd
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2026-04-06 20:46:29 +0200

    fix main file & add some books paths

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from stats import get_num_words, get_characters, sorted_dict
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    book_path = sys.argv[1]
 
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
 
     return contents
-book_path = 'books/frankenstein.txt'
+
 text = get_book_text(book_path)
 
 print("============ BOOKBOT ============")
@@ -19,3 +26,5 @@ character = get_characters(text)
 for item in sorted_dict(character):
     if item["char"].isalpha():
         print(f'{item["char"]}: {item["num"]}')
+
+print("============= END ===============")
